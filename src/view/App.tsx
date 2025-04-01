a2d52d7d74682ee8cceeeddb03096550b91f2bda
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2025-04-01 19:46:32 +0200

    Squashed commit of the following:
    commit 21fc99163d718e22acf6cc2fa8f0e5432a6ddd73
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Fri Nov 15 15:59:26 2024 +0100
    
        Update readme
    
    commit 46f3d4f2f8c2704bfd8123b00c76b52d9e7604d0
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 21:04:33 2024 +0100
    
        Fix build
    
    commit 5804c219a19243a07a3806356ced64acdc7e465d
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 21:04:27 2024 +0100
    
        Tailwind functions
    
    commit 0cad888445187084553a08fd4530ea86521f2616
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 20:58:41 2024 +0100
    
        Prettier tailwind must come last
    
    commit 4d9feca81a6d5e1288a59d7e31df16587e45a84f
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 01:11:21 2024 +0100
    
        Move files
    
    commit 97140d8aa8115a34c1f332be1802feaed8748bf5
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 01:03:06 2024 +0100
    
        Tailwind intellisense in cva
    
    commit 6b6cae8c31104992e71e5d9785f7fabe2324837f
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 01:00:37 2024 +0100
    
        Sort imports
    
    commit 640faca1034d7773f1f91ab4c56784b022190eb1
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 00:55:50 2024 +0100
    
        Extract button variants
    
    commit 3d5a515f56ce4482a8af493ea0fab8757ba3b3b7
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 00:55:50 2024 +0100
    
        Add button
    
    commit fb74ff1f1834a5172f17c948d12f9003c4d57e9c
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 00:55:50 2024 +0100
    
        Modify shadcn file structure
    
    commit 76b935d818d6eb4696cab8adce1d50b3cd547be4
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 00:55:50 2024 +0100
    
        Initialize tailwind
    
    commit c4b5f16ca389e9060bbf44a742dfe798de6d099a
    Author: Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
    Date:   Thu Nov 14 00:55:49 2024 +0100
    
        Add aliases

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from './components/shadcn/button';
 import { useCounter } from '@/shell/useCounter';
 
 function App() {
@@ -10,29 +11,32 @@ function App() {
 
       {/* Button grid container */}
       <div className="grid w-full grid-cols-2 gap-4">
-        {/* Increment button */}
-        <button
-          className="rounded-lg bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600 focus:ring-2 focus:ring-blue-400 focus:outline-none"
+        <Button
+          size={'lg'}
+          variant={'default'}
+          className="text-md font-semibold"
           onClick={onIncrement}
         >
           Increment
-        </button>
+        </Button>
 
-        {/* Decrement button */}
-        <button
-          className="rounded-lg bg-red-500 px-4 py-2 font-semibold text-white hover:bg-red-600 focus:ring-2 focus:ring-red-400 focus:outline-none"
+        <Button
+          size={'lg'}
+          variant={'default'}
+          className="text-md font-semibold"
           onClick={onDecrement}
         >
           Decrement
-        </button>
+        </Button>
 
-        {/* Increment Async button, spanning two columns */}
-        <button
-          className="col-span-2 rounded-lg bg-green-500 px-4 py-2 font-semibold text-white hover:bg-green-600 focus:ring-2 focus:ring-green-400 focus:outline-none"
+        <Button
+          size={'lg'}
+          variant={'default'}
+          className="text-md col-span-2 font-semibold"
           onClick={onIncrementAsync}
         >
           Increment Async
-        </button>
+        </Button>
       </div>
     </div>
   );
