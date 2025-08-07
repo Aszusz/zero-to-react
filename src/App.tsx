4bc311f286a6d5cd43ae07f508cd07759c352244
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2025-08-07 13:35:23 +0200

    chore: update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
       <h1 className="text-2xl font-semibold">Counter: {count}</h1>
       <div className="flex space-x-4">
         <button
-          className="rounded-lg bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="rounded-lg bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600 focus:ring-2 focus:ring-blue-400 focus:outline-none"
           onClick={() => setCount((count) => count + 1)}
         >
           Increment
         </button>
         <button
-          className="rounded-lg bg-red-500 px-4 py-2 font-semibold text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+          className="rounded-lg bg-red-500 px-4 py-2 font-semibold text-white hover:bg-red-600 focus:ring-2 focus:ring-red-400 focus:outline-none"
           onClick={() => setCount((count) => count - 1)}
         >
           Decrement
