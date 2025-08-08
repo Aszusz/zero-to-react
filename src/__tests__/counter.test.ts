2b6bafed60137474f38b785ee6d1d3188f09c0dd
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2025-08-08 12:26:54 +0200

    fix: tests

--- src/__tests__/counter.test.ts
+++ src/__tests__/counter.test.ts
@@ -1,4 +1,4 @@
-import { AC } from '@/core/actions';
+import { Actions } from '@/core/actions';
 import { reducer } from '@/core/reducer';
 import { selectCount } from '@/core/selectors';
 import { initialState } from '@/core/state';
@@ -13,22 +13,22 @@ describe('Counter Functionality', () => {
 
   it('should increment the count by 1 when increment action is dispatched', () => {
     let state = initialState;
-    state = reducer(state, AC['ui/increment'](null));
+    state = reducer(state, Actions.create['ui/increment'](null));
     const count = selectCount(state);
     expect(count).toBe(1);
   });
 
   it('should decrement the count by 1 when decrement action is dispatched', () => {
     let state = initialState;
-    state = reducer(state, AC['ui/decrement'](null));
+    state = reducer(state, Actions.create['ui/decrement'](null));
     const count = selectCount(state);
     expect(count).toBe(-1);
   });
 
   it('should increment the count by the specified amount when async increment action completes', () => {
     let state = initialState;
-    state = reducer(state, AC['ui/increment-async'](null));
-    state = reducer(state, AC['eff/increment-async-ready'](7));
+    state = reducer(state, Actions.create['ui/increment-async'](null));
+    state = reducer(state, Actions.create['eff/increment-async-ready'](7));
     const count = selectCount(state);
     expect(count).toBe(7);
   });
